2c1d26911e28d5dead345eeb2e2115bab63619db
FonceurOk <chrismongombe50@gmail.com>
2026-05-12 15:49:18 +0200

    start setup quizz

---
+++ src/app/Models/Quizz.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Quizz extends Model
+{
+    use HasFactory;
+
+    protected $fillable = array('title', 'logo_url');
+
+    public function getUsers(): BelongsToMany {
+        return $this->belongsToMany(User::class);
+    }
+}
